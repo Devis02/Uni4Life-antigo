1f7c53c7c656b3a06f5fcefc26652d1af949af27
Guilherme Moraes <140280852+Guicmoraes@users.noreply.github.com>
2024-07-01 21:25:00 -0300

    Content.java

---
+++ Content.java
@@ -0,0 +1,10 @@
+package Uni4Life;
+import java.util.Date;
+public class Content {
+	public String contentName;
+	public Account author;
+	public Double value;
+	public Date publishDate;
+	public String type;
+	
+}
