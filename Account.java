c21d7c6340b4a554cdecdf2ae91121c8204c0a92
Guilherme Moraes <140280852+Guicmoraes@users.noreply.github.com>
2024-07-01 21:24:28 -0300

    Account.java

---
+++ Account.java
@@ -0,0 +1,40 @@
+package Uni4Life;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Account {
+	private String accountName;
+	private String password;
+	private Double balance;
+	private List<Content> contentLibrary = new ArrayList<>();
+	
+	public Account(String name,String password) {
+		this.accountName = name;
+		this.password = password;
+	}
+	
+	public void buyContent(Content content) {
+		this.balance -= content.value;
+		contentLibrary.add(content);
+	}
+	
+	public String getName() {
+		return accountName;
+	}
+	
+	public void addCash(Double cash) {
+		this.balance +=cash;
+	}
+	
+	public void setPassword(String newPassword) {
+		this.password = newPassword;
+	}
+	public Boolean verification(String passwordVerification) {
+		if(passwordVerification == this.password) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
