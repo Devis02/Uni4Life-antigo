5fff22a2383ce2449a1b4a25a2584a933b133bd1
Guilherme Moraes <140280852+Guicmoraes@users.noreply.github.com>
2024-07-08 22:53:49 -0300

    Update Content.java

--- Content.java
+++ Content.java
@@ -7,4 +7,17 @@ public class Content {
 	public Date publishDate;
 	public String type;
 	
+	
+	public Content(String contentName, Account author, Double value, Date publishDate, String type) {
+		this.contentName = contentName;
+		this.author = author;
+		this.value = value;
+		this.publishDate = publishDate;
+		this.type = type;
+	}
+	
+	public String toString(){
+		return String.format("Content name: %s%nAuthor name: %s%nPrice: R$%.2f%nType of content: %s%nPublish date: ",this.contentName,this.author.getName(),this.value,this.type) + this.publishDate;  
+	}
 }
+
