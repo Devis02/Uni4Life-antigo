699f2115da3daa4d6689ae367d6936430cc421d2
Guilherme Moraes <140280852+Guicmoraes@users.noreply.github.com>
2024-07-10 15:15:56 -0300

    Update Uni4LifeMain.java

--- Uni4LifeMain.java
+++ Uni4LifeMain.java
@@ -4,32 +4,79 @@ import java.util.Date;
 import java.util.Locale;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
+import java.util.Scanner;
 public class Uni4LifeMain {
-	public static void main(String[] args) throws ParseException{
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		Locale.setDefault(Locale.US);
-		Date publishDate = simpleDateFormat.parse("23/03/2024");
-		
-		Account author = new Account("Jorge","123445");
-		Content content = new Content("CÁLCULO I: Uma breve introdução",author,10.50,publishDate,"Artigo");
-		Content SoftwareEngineerContent = new Content("Engenharia de Software: Padrões de Projeto",author,0.0,publishDate,"Artigo");
 	
-		Account student = new Account("Maria","123456");
+	public static void main(String[] args) throws ParseException{
 		
-		student.addCash(20.00);
-		System.out.println("Balance: "+student.getBalance());
+		Scanner sc = new Scanner(System.in);
+		AccountHolder accountHolder = new AccountHolder();
+		Boolean running = true;
+		Boolean loggedIn = false;
 		
-		System.out.println("[Buy content Use Case]");
-		student.buyContent(content);
-		System.out.println("Balance: "+student.getBalance());
-
-		student.buyContent(SoftwareEngineerContent);
-		System.out.println(student.getContentLibrary());
-		System.out.println("Balance: "+student.getBalance());
+		do {
+			Integer choice = showFunctionalities(sc);
+			
+			switch(choice) {
+			
+			case 1:
+				
+				System.out.print("Digite um nome para sua conta: ");
+				String accountName = sc.next();
+				
+				System.out.print("Digite uma senha para sua conta: ");
+				String accountPassword = sc.next();
+				
+				Account academicAccount = createAccount(accountName,accountPassword);
+				saveAccount(accountHolder,academicAccount);
+				break;
+			case 2:
+				
+				System.out.print("Digite o nome de sua conta: ");
+				accountName = sc.next();
+				
+				System.out.print("Digite a senha de sua conta: ");
+				accountPassword = sc.next();
+				Account accountAccess = createAccount(accountName,accountPassword);
+				if(accountHolder.verifyAccountInTheList(accountAccess,accountPassword)==true) {
+					System.out.println("Entrando...");
+					loggedIn = true;
+				}
+				else {
+					System.out.println("Valores inválidos");
+				}
+				break;
+			case 3:
+				
+				running = false;
+				break;
+			}
+		}
+		while(running != false && loggedIn == false);
 		
-		System.out.println("[Find Content Use Case]");
-		System.out.println(student.getContentLibrary().get(student.getContentLibrary().indexOf(SoftwareEngineerContent)));
+		sc.close();
+	}
+	
+	public static Account createAccount(String accountName, String accountPassword) {
+		
+		Account academicAccount = new Account(accountName,accountPassword);
+		return academicAccount;
 		
 	}
+	
+	public static void saveAccount(AccountHolder accountHolder,Account academicAccount) {
+		accountHolder.addAcountToTheList(academicAccount);
+		
+	}
+
+	public static void realizeLogin() {
+		
+	}
+	
+	public static Integer showFunctionalities(Scanner sc) {
+		System.out.printf("[1] - Criar nova conta%n[2] - Realizar Login%n[3] - Sair%nDigite uma opção: ");
+		Integer choice = sc.nextInt();
+		return choice;
+	}
+	
 }
